0e798480fd84b8f8bc4504eae83d33c527d88060
a.choplin <achoplin@gmail.com>
2023-06-27 21:02:29 +0200

    [FEATURE] Embedding generation

--- back/prisma/seed.js
+++ back/prisma/seed.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
 const prisma = new PrismaClient()
 
 async function main() {
@@ -7,14 +9,14 @@ async function main() {
     const robert = await prisma.user.findUnique({
         where: {
             email: 'robert@girafe.ia',
-        }}).then((user) => {
+        }}).then(async (user) => {
             if (!user) {
                 return prisma.user.create({
                     data: {
                         firstname: 'Robert',
                         lastname: 'Girafe',
                         email: 'robert@girafe.ia',
-                        password: '123soleil!',
+                        password: await bcrypt.hash("123", 10),
                     },
                 });
             }
@@ -23,7 +25,7 @@ async function main() {
     const group1 = await prisma.group.create({
         data: {
             name: 'Group 1',
-            directory: '',
+            directory: 'documents/RobertGirafe/',
             userId: 1,
         },
     });
@@ -31,28 +33,28 @@ async function main() {
     const document1 = await prisma.document.create({
         data: {
             name: 'Document 1',
-            directory: "C:/Users/Clo/Downloads/documents/BUT_Plaquette_A5_INFO.pdf",
+            directory: "documents/RobertGirafe/docs/BUT_Plaquette_A5_INFO.pdf",
             groupId: 1,
         },
     });
     const document2 = await prisma.document.create({
         data: {
             name: 'Document 2',
-            directory: "C:/Users/Clo/Downloads/documents/ContratdapprentissageAgenGradignanMAJ300321.pdf",
+            directory: "documents/RobertGirafe/docs/ContratdapprentissageAgenGradignanMAJ300321.pdf",
             groupId: 1,
         },
     });
     const document3 = await prisma.document.create({
         data: {
             name: 'Document 3',
-            directory: "C:/Users/Clo/Downloads/documents/PRO-18-01V03LeContratdeProfessionnalisationAntenneAgenGradignan.pdf",
+            directory: "documents/RobertGirafe/docs/PRO-18-01V03LeContratdeProfessionnalisationAntenneAgenGradignan.pdf",
             groupId: 1,
         },
     });
     const document4 = await prisma.document.create({
         data: {
             name: 'Document 4',
-            directory: "C:/Users/Clo/Downloads/documents/PRO-44-01-Aide-Exceptionnelle-2021-AntenneAgen-Gradignan-V01-02-04-21.pdf",
+            directory: "documents/RobertGirafe/docs/PRO-44-01-Aide-Exceptionnelle-2021-AntenneAgen-Gradignan-V01-02-04-21.pdf",
             groupId: 1,
         },
     });
