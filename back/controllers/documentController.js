654378df9396b98d1fd312b5a5f1c7bbbb6fa307
a.choplin <achoplin@gmail.com>
2023-06-29 09:39:06 +0200

    [FEATURE] Get All Exchanges of user

--- back/controllers/documentController.js
+++ back/controllers/documentController.js
@@ -21,3 +21,28 @@ export async function getDocuments(req, res) {
   //   res.status(500).send("Error processing request");
   // }
 }
+
+export async function getExchange(req, res) {
+  const userId = req.session.userId;
+  const prisma = new PrismaClient();
+  // if (userId != "undefined" && userId != "" && userId != null) {
+  try {
+    const exchanges = await prisma.exchange.findMany({
+      where: {
+        userId: 1,
+        // userId: parseInt(userId),
+      },
+      include: {
+        documents: true,
+      },
+    });
+
+    res.status(200).json(exchanges);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error processing request");
+  }
+    // } else {
+  //   res.status(500).send("Error processing request");
+  // }
+}
--- back/routes/documentRoutes.js
+++ back/routes/documentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Router } from 'express';
-import { getDocuments } from '../controllers/documentController.js';
+import { getDocuments, getExchange } from '../controllers/documentController.js';
 const router = Router();
 
 // Add middleware function to parse the request body as JSON
@@ -8,5 +8,7 @@ router.use(express.json());
 
 // Add GET route for embedding
 router.get('/get', getDocuments);
+
+router.get('/Exchange/get', getExchange);
 
 export default router;
