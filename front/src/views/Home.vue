9feb98963f00c468e043925657d161a3b1835f02
Bedouret Romain <romain.bedouret@etu.u-bordeaux.fr>
2023-06-29 17:53:46 +0200

    Historique

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <TopHeader />
+  <v-container fluid class="bg-blue">
+    <v-row class="d-flex justify-center align-center vh-100">
+      <v-col cols="12" md="8">
+        <v-card class="text-center">
+          <v-card-text>
+            Salut, ça va ?
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+
 
 <script setup>
   // import HelloWorld from '@/components/HelloWorld.vue'
