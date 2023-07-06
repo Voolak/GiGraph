3700e3d08854ce4d9cc0846960c02e37f63aec9b
Magnien Cloe <cloe.magnien@u-bordeaux.fr>
2023-07-06 09:53:27 +0200

    smol fix

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -47,7 +47,7 @@
                   style="height: 30px"></b-col>
               <b-col>
                 <p v-show="message.loading == false">
-                <div :id="'graph' + (message.id).toString()" style="width: 800px;height:400px;"></div>
+                  <div :id="'graph' + (message.id).toString()" style="width: 800px;height:400px;"></div>
                 </p>
                 <p v-show="message.loading == true"><v-progress-circular color="primary"
                     indeterminate></v-progress-circular></p>
@@ -142,9 +142,8 @@
           }
           documents.value.push([line[0].name, processed]);
         });
-        console.log(hasToTreatDocs.value);
+        
         shouldTreatDocs();
-        console.log(hasToTreatDocs.value);
         
         showdocuments.value = documents.value.length > 0;
       }).catch(error => {
@@ -152,268 +151,268 @@
       });
   });
 
-async function postMessage() {
-  if (question.value != "") {
-    var id = counter.value
-    loading.value = true;
-    messages.value.push({ 'message': question.value, 'type': 0 });
-    messages.value.push({ 'type': 1, 'loading': true, 'id': id });
-    counter.value++
-    question.value = ""
+  async function postMessage() {
+    if (question.value != "") {
+      var id = counter.value
+      loading.value = true;
+      messages.value.push({ 'message': question.value, 'type': 0 });
+      messages.value.push({ 'type': 1, 'loading': true, 'id': id });
+      counter.value++
+      question.value = ""
 
-    await new Promise(r => setTimeout(r, 2000));
+      await new Promise(r => setTimeout(r, 2000));
 
-    const lastMessage = messages.value[messages.value.length - 1];
-    lastMessage.loading = false;
-    loading.value = false;
-    console.log('graph' + (counter.value - 1).toString())
-    var myChart = echarts.init(document.getElementById('graph' + (counter.value - 1).toString()));
-    var option;
-    if (counter.value == 1) {
-      option = {
-        title: {
-          text: 'ECharts Getting Started Example'
-        },
-        tooltip: {},
-        legend: {
-          data: ['sales']
-        },
-        xAxis: {
-          data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
-        },
-        yAxis: {},
-        series: [
-          {
-            name: 'sales',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      };
-    } if (counter.value == 2) {
-      option = {
-        title: {
-          text: 'Referer of a Website',
-          subtext: 'Fake Data',
-          left: 'center'
-        },
-        tooltip: {
-          trigger: 'item'
-        },
-        legend: {
-          orient: 'vertical',
-          left: 'left'
-        },
-        series: [
-          {
-            name: 'Access From',
-            type: 'pie',
-            radius: '50%',
-            data: [
-              { value: 1048, name: 'Search Engine' },
-              { value: 735, name: 'Direct' },
-              { value: 580, name: 'Email' },
-              { value: 484, name: 'Union Ads' },
-              { value: 300, name: 'Video Ads' }
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
+      const lastMessage = messages.value[messages.value.length - 1];
+      lastMessage.loading = false;
+      loading.value = false;
+      console.log('graph' + (counter.value - 1).toString())
+      var myChart = echarts.init(document.getElementById('graph' + (counter.value - 1).toString()));
+      var option;
+      if (counter.value == 1) {
+        option = {
+          title: {
+            text: 'ECharts Getting Started Example'
+          },
+          tooltip: {},
+          legend: {
+            data: ['sales']
+          },
+          xAxis: {
+            data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+          },
+          yAxis: {},
+          series: [
+            {
+              name: 'sales',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+            }
+          ]
+        };
+      } if (counter.value == 2) {
+        option = {
+          title: {
+            text: 'Referer of a Website',
+            subtext: 'Fake Data',
+            left: 'center'
+          },
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            orient: 'vertical',
+            left: 'left'
+          },
+          series: [
+            {
+              name: 'Access From',
+              type: 'pie',
+              radius: '50%',
+              data: [
+                { value: 1048, name: 'Search Engine' },
+                { value: 735, name: 'Direct' },
+                { value: 580, name: 'Email' },
+                { value: 484, name: 'Union Ads' },
+                { value: 300, name: 'Video Ads' }
+              ],
+              emphasis: {
+                itemStyle: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
               }
             }
-          }
-        ]
-      };
-    } if (counter.value == 3) {
-      option = {
-        color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
-        title: {
-          text: 'Gradient Stacked Area Chart'
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            label: {
-              backgroundColor: '#6a7985'
+          ]
+        };
+      } if (counter.value == 3) {
+        option = {
+          color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
+          title: {
+            text: 'Gradient Stacked Area Chart'
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#6a7985'
+              }
             }
-          }
-        },
-        legend: {
-          data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']
-        },
-        toolbox: {
-          feature: {
-            saveAsImage: {}
-          }
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [
-          {
-            type: 'category',
-            boundaryGap: false,
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: 'Line 1',
-            type: 'line',
-            stack: 'Total',
-            smooth: true,
-            lineStyle: {
-              width: 0
-            },
-            showSymbol: false,
-            areaStyle: {
-              opacity: 0.8,
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                {
-                  offset: 0,
-                  color: 'rgb(128, 255, 165)'
-                },
-                {
-                  offset: 1,
-                  color: 'rgb(1, 191, 236)'
-                }
-              ])
-            },
-            emphasis: {
-              focus: 'series'
-            },
-            data: [140, 232, 101, 264, 90, 340, 250]
           },
-          {
-            name: 'Line 2',
-            type: 'line',
-            stack: 'Total',
-            smooth: true,
-            lineStyle: {
-              width: 0
-            },
-            showSymbol: false,
-            areaStyle: {
-              opacity: 0.8,
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                {
-                  offset: 0,
-                  color: 'rgb(0, 221, 255)'
-                },
-                {
-                  offset: 1,
-                  color: 'rgb(77, 119, 255)'
-                }
-              ])
-            },
-            emphasis: {
-              focus: 'series'
-            },
-            data: [120, 282, 111, 234, 220, 340, 310]
+          legend: {
+            data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']
           },
-          {
-            name: 'Line 3',
-            type: 'line',
-            stack: 'Total',
-            smooth: true,
-            lineStyle: {
-              width: 0
-            },
-            showSymbol: false,
-            areaStyle: {
-              opacity: 0.8,
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                {
-                  offset: 0,
-                  color: 'rgb(55, 162, 255)'
-                },
-                {
-                  offset: 1,
-                  color: 'rgb(116, 21, 219)'
-                }
-              ])
-            },
-            emphasis: {
-              focus: 'series'
-            },
-            data: [320, 132, 201, 334, 190, 130, 220]
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
           },
-          {
-            name: 'Line 4',
-            type: 'line',
-            stack: 'Total',
-            smooth: true,
-            lineStyle: {
-              width: 0
-            },
-            showSymbol: false,
-            areaStyle: {
-              opacity: 0.8,
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                {
-                  offset: 0,
-                  color: 'rgb(255, 0, 135)'
-                },
-                {
-                  offset: 1,
-                  color: 'rgb(135, 0, 157)'
-                }
-              ])
-            },
-            emphasis: {
-              focus: 'series'
-            },
-            data: [220, 402, 231, 134, 190, 230, 120]
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
           },
-          {
-            name: 'Line 5',
-            type: 'line',
-            stack: 'Total',
-            smooth: true,
-            lineStyle: {
-              width: 0
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: false,
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ],
+          series: [
+            {
+              name: 'Line 1',
+              type: 'line',
+              stack: 'Total',
+              smooth: true,
+              lineStyle: {
+                width: 0
+              },
+              showSymbol: false,
+              areaStyle: {
+                opacity: 0.8,
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: 'rgb(128, 255, 165)'
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(1, 191, 236)'
+                  }
+                ])
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: [140, 232, 101, 264, 90, 340, 250]
             },
-            showSymbol: false,
-            label: {
-              show: true,
-              position: 'top'
+            {
+              name: 'Line 2',
+              type: 'line',
+              stack: 'Total',
+              smooth: true,
+              lineStyle: {
+                width: 0
+              },
+              showSymbol: false,
+              areaStyle: {
+                opacity: 0.8,
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: 'rgb(0, 221, 255)'
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(77, 119, 255)'
+                  }
+                ])
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: [120, 282, 111, 234, 220, 340, 310]
             },
-            areaStyle: {
-              opacity: 0.8,
-              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                {
-                  offset: 0,
-                  color: 'rgb(255, 191, 0)'
-                },
-                {
-                  offset: 1,
-                  color: 'rgb(224, 62, 76)'
-                }
-              ])
+            {
+              name: 'Line 3',
+              type: 'line',
+              stack: 'Total',
+              smooth: true,
+              lineStyle: {
+                width: 0
+              },
+              showSymbol: false,
+              areaStyle: {
+                opacity: 0.8,
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: 'rgb(55, 162, 255)'
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(116, 21, 219)'
+                  }
+                ])
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: [320, 132, 201, 334, 190, 130, 220]
             },
-            emphasis: {
-              focus: 'series'
+            {
+              name: 'Line 4',
+              type: 'line',
+              stack: 'Total',
+              smooth: true,
+              lineStyle: {
+                width: 0
+              },
+              showSymbol: false,
+              areaStyle: {
+                opacity: 0.8,
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: 'rgb(255, 0, 135)'
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(135, 0, 157)'
+                  }
+                ])
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: [220, 402, 231, 134, 190, 230, 120]
             },
-            data: [220, 302, 181, 234, 210, 290, 150]
-          }
-        ]
-      };
+            {
+              name: 'Line 5',
+              type: 'line',
+              stack: 'Total',
+              smooth: true,
+              lineStyle: {
+                width: 0
+              },
+              showSymbol: false,
+              label: {
+                show: true,
+                position: 'top'
+              },
+              areaStyle: {
+                opacity: 0.8,
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: 'rgb(255, 191, 0)'
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(224, 62, 76)'
+                  }
+                ])
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: [220, 302, 181, 234, 210, 290, 150]
+            }
+          ]
+        };
+      }
+
+      // Display the chart using the configuration items and data just specified.
+      myChart.setOption(option);
     }
-
-
-    // Display the chart using the configuration items and data just specified.
-    myChart.setOption(option);
   }
 
   function ajouterDocument() {
@@ -488,53 +487,53 @@ async function postMessage() {
 </script>
 
 <style>
-.centered-content {
-  display: flex;
-  align-items: center;
-}
+  .centered-content {
+    display: flex;
+    align-items: center;
+  }
 
-.align-right {
-  margin-left: auto;
-}
+  .align-right {
+    margin-left: auto;
+  }
 
 
-.chat {
-  background-color: rgba(33, 150, 243, 0.1);
-}
+  .chat {
+    background-color: rgba(33, 150, 243, 0.1);
+  }
 
-.custom-label .v-label {
-  opacity: 1 !important;
-}
+  .custom-label .v-label {
+    opacity: 1 !important;
+  }
 
-.messages {
-  padding: 5vh 5vw;
-  height: calc(100vh - 125px);
-  overflow: auto;
-}
+  .messages {
+    padding: 5vh 5vw;
+    height: calc(100vh - 125px);
+    overflow: auto;
+  }
 
-.ask {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 50vw;
-  position: absolute;
-  bottom: 5vh;
-  left: 32vw;
-}
+  .ask {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50vw;
+    position: absolute;
+    bottom: 5vh;
+    left: 32vw;
+  }
 
-.select {
-  width: 150px;
-  max-height: 150px;
-  font-size: 11px;
-  height: 100px;
-}
+  .select {
+    width: 150px;
+    max-height: 150px;
+    font-size: 11px;
+    height: 100px;
+  }
 
-p {
-  position: relative;
-  background-color: #fff;
-  padding: 1.125em 1.5em;
-  font-size: 1em;
-  border-radius: 1rem;
-  box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, .3), 0 0.0625rem 0.125rem rgba(0, 0, 0, .2);
-}
+  p {
+    position: relative;
+    background-color: #fff;
+    padding: 1.125em 1.5em;
+    font-size: 1em;
+    border-radius: 1rem;
+    box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, .3), 0 0.0625rem 0.125rem rgba(0, 0, 0, .2);
+  }
 </style>
