9fa491bff4d9a95ee479d357c6d4fb6fa20e6d3b
Magnien Cloe <cloe.magnien@u-bordeaux.fr>
2023-07-06 12:27:12 +0200

    fix front

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -6,7 +6,7 @@
                 <v-list color="transparent">
                     <v-list-item prepend-icon="mdi-account-box" title="Robert Girafe" class="mb-7"></v-list-item>
                     <v-file-input prepend-icon="" v-model="files" label="AJOUTER UN DOCUMENT" variant="solo-filled" 
-                    accept=".pdf,.csv,.sql" @change="ajouterDocument" class="custom-label"></v-file-input>
+                      accept=".pdf,.csv,.sql" @change="ajouterDocument" class="custom-label"></v-file-input>
                     <v-card class="mx-auto" max-width="400">
                         <v-list v-show="showdocuments">
                             <v-card v-for="(document, index) in documents" :key="index" class="mx-auto" max-width="400">
@@ -26,7 +26,7 @@
                             </v-card>
                         </v-list>
                     </v-card>
-                    <div v-if="hasToTreatDocs" :style="{fontSize: '17px', padding: '3px', backgroundColor: 'black', margin: 'auto', opacity: '60%', width: '90%', borderRadius: '5px', color: 'red', textAlign: 'center', marginTop: '10px'}">Vous avez des documents à traiter.</div>
+                    <div v-if="hasToTreatDocs" :style="{fontSize: '17px', padding: '3px', backgroundColor: 'white', margin: 'auto', opacity: '60%', width: '90%', borderRadius: '5px', color: 'red', textAlign: 'center', marginTop: '10px'}">Vous avez des documents à traiter.</div>
                     <v-btn v-show="hasToTreatDocs" class="my-5" @click="traiterDocuments()">
                         Traiter les documents
                     </v-btn>
@@ -40,15 +40,18 @@
           </div>
         </template>
       </v-navigation-drawer>
-      <v-main style="height: 100vh; background-color: white">
+      <v-main style="height: 100vh">
         <div class="messages">
-          <div v-for="message in messages" class="mb-12">
+          <div v-show="showdocuments" v-for="message in messages" class="mb-12">
             <v-row v-if="message.type == 1">
               <b-col class="text-right"><img class="mx-5 mb-2" src="../assets/logo.png" alt="logo"
                   style="height: 30px"></b-col>
               <b-col>
                 <p v-show="message.loading == false">
-                  <div :id="'graph' + (message.id).toString()" style="width: 800px;height:400px;"></div>
+                <div v-if="message.isMessage == true">{{ message.message }}</div>
+                <div v-else :id="'graph' + (message.id).toString()" style="width: 800px;height:400px;">
+                </div>
+
                 </p>
                 <p v-show="message.loading == true"><v-progress-circular color="primary"
                     indeterminate></v-progress-circular></p>
@@ -62,33 +65,15 @@
 
             </v-row>
           </div>
-
+          <div v-show="!showdocuments"
+            style="font-size: 20px; align-items: center; display: flex; justify-content: center; margin-top: 40vh">
+            Veuillez
+            ajouter des documents avant de poser une question
+          </div>
         </div>
 
-        <v-form validate-on="submit lazy" @submit.prevent="submit" style="width: ;">
+        <v-form v-if="showdocuments" validate-on="submit lazy" @submit.prevent="submit" style="width: ;">
           <v-row class="ask" align="center" justify="center">
-            <!-- <div class="col">
-                            <v-select :items="items2" item-value="value" item-text="text"
-                                :menu-props="{ offsetY: true, closeOnContentClick: false }">
-                                <template #selection="{ item }">
-                                    <v-img src="../assets/logo.png" max-height="20" max-width="20"></v-img>
-                                </template>
-                                <template #item="{ item }">
-                                    <v-list-item-avatar>
-                                        <v-img src="../assets/logo.png" max-height="20" max-width="20"></v-img>
-                                    </v-list-item-avatar>
-                                    <v-list-item-content>
-                                        <v-list-item-title>{{ item.value }}</v-list-item-title>
-                                    </v-list-item-content>
-                                </template>
-                            </v-select>
-                        </div> -->
-            <!-- <v-textarea class="mx-12" counter label="Posez votre question ici" maxlength="50" single-line
-                        v-model="question"></v-textarea> -->
-            <div class="col chat">
-              <v-select label="Select" class="select"
-                :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
-            </div>
             <v-textarea v-model="question" hide-details class="mx-14 chat " variant="filled" auto-grow
               label="Envoyer un message" rows="2" row-height="20"></v-textarea>
             <v-col cols="auto">
@@ -154,22 +139,34 @@
 
   async function postMessage() {
     if (question.value != "") {
-      var id = counter.value
+      var id = counter.value;
       loading.value = true;
       messages.value.push({ 'message': question.value, 'type': 0 });
       messages.value.push({ 'type': 1, 'loading': true, 'id': id });
-      counter.value++
-      question.value = ""
+      counter.value++;
+      
+      const options = {
+        method: 'POST',
+        url: 'http://127.0.0.1:3000/backend/agent',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: {
+          prompt: question.value
+        }
+      };
 
-      await new Promise(r => setTimeout(r, 2000));
-
-      const lastMessage = messages.value[messages.value.length - 1];
-      lastMessage.loading = false;
-      loading.value = false;
-      console.log('graph' + (counter.value - 1).toString())
-      if (graph.type) {
-        myChart = echarts.init(document.getElementById('graph' + (counter.value - 1).toString()));
-      }
+      axios.request(options).then(function (response) {
+        const graph = response.data.response;
+        console.log(graph);
+        const lastMessage = messages.value[messages.value.length - 1];
+        lastMessage.loading = false;
+        loading.value = false;
+        question.value = "";
+        var myChart;
+        var option;
+        console.log('graph' + (counter.value - 1).toString());
+        if (graph.type) {
+          myChart = echarts.init(document.getElementById('graph' + (counter.value - 1).toString()));
+        }
 
       console.log("Graph type :" + graph.type)
       switch (graph.type) {
@@ -180,20 +177,20 @@
             }
           },
           title: {
-            text: 'ECharts Getting Started Example'
+            text: graph.title
           },
           tooltip: {},
           legend: {
-            data: ['sales']
+            data: [graph.name]
           },
           xAxis: {
-            data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+            data: graph.columnsName
           },
           yAxis: {},
           series: [
             {
               type: 'bar',
-              data: [5, 20, 36, 10, 10, 20]
+              data: graph.values
             }
           ]
         };
@@ -207,8 +204,8 @@
             }
           },
           title: {
-            text: 'Referer of a Website',
-            subtext: 'Fake Data',
+            text: graph.title,
+            subtext: graph.subtitle,
             left: 'center'
           },
           tooltip: {
@@ -218,13 +215,7 @@
             {
               type: 'pie',
               radius: '50%',
-              data: [
-                { value: 1048, name: 'Search Engine' },
-                { value: 735, name: 'Direct' },
-                { value: 580, name: 'Email' },
-                { value: 484, name: 'Union Ads' },
-                { value: 300, name: 'Video Ads' }
-              ],
+              data: graph.datas,
               emphasis: {
                 itemStyle: {
                   shadowBlur: 10,
@@ -249,36 +240,6 @@
             boundaryGap: false,
             data: graph.columnsName
           },
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'cross',
-              label: {
-                backgroundColor: '#6a7985'
-              }
-            }
-          },
-          legend: {
-            data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']
-          },
-          toolbox: {
-            feature: {
-              saveAsImage: {}
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis: [
-            {
-              type: 'category',
-              boundaryGap: false,
-              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            }
-          ],
           yAxis: [
             {
               type: 'value'
@@ -286,156 +247,36 @@
           ],
           series: [
             {
-              name: 'Line 1',
+              data: graph.values,
               type: 'line',
-              stack: 'Total',
-              smooth: true,
-              lineStyle: {
-                width: 0
-              },
-              showSymbol: false,
-              areaStyle: {
-                opacity: 0.8,
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  {
-                    offset: 0,
-                    color: 'rgb(128, 255, 165)'
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgb(1, 191, 236)'
-                  }
-                ])
-              },
-              emphasis: {
-                focus: 'series'
-              },
-              data: [140, 232, 101, 264, 90, 340, 250]
-            },
-            {
-              name: 'Line 2',
-              type: 'line',
-              stack: 'Total',
-              smooth: true,
-              lineStyle: {
-                width: 0
-              },
-              showSymbol: false,
-              areaStyle: {
-                opacity: 0.8,
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  {
-                    offset: 0,
-                    color: 'rgb(0, 221, 255)'
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgb(77, 119, 255)'
-                  }
-                ])
-              },
-              emphasis: {
-                focus: 'series'
-              },
-              data: [120, 282, 111, 234, 220, 340, 310]
-            },
-            {
-              name: 'Line 3',
-              type: 'line',
-              stack: 'Total',
-              smooth: true,
-              lineStyle: {
-                width: 0
-              },
-              showSymbol: false,
-              areaStyle: {
-                opacity: 0.8,
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  {
-                    offset: 0,
-                    color: 'rgb(55, 162, 255)'
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgb(116, 21, 219)'
-                  }
-                ])
-              },
-              emphasis: {
-                focus: 'series'
-              },
-              data: [320, 132, 201, 334, 190, 130, 220]
-            },
-            {
-              name: 'Line 4',
-              type: 'line',
-              stack: 'Total',
-              smooth: true,
-              lineStyle: {
-                width: 0
-              },
-              showSymbol: false,
-              areaStyle: {
-                opacity: 0.8,
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  {
-                    offset: 0,
-                    color: 'rgb(255, 0, 135)'
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgb(135, 0, 157)'
-                  }
-                ])
-              },
-              emphasis: {
-                focus: 'series'
-              },
-              data: [220, 402, 231, 134, 190, 230, 120]
-            },
-            {
-              name: 'Line 5',
-              type: 'line',
-              stack: 'Total',
-              smooth: true,
-              lineStyle: {
-                width: 0
-              },
-              showSymbol: false,
-              label: {
-                show: true,
-                position: 'top'
-              },
-              areaStyle: {
-                opacity: 0.8,
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  {
-                    offset: 0,
-                    color: 'rgb(255, 191, 0)'
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgb(224, 62, 76)'
-                  }
-                ])
-              },
-              emphasis: {
-                focus: 'series'
-              },
-              data: [220, 302, 181, 234, 210, 290, 150]
+              areaStyle: {}
             }
           ]
         };
-      }
+          // Display the chart using the configuration items and data just specified.
+          myChart.setOption(option);
+          break;
+        default:
+          console.log(response);
+          question.value = "";
+          messages.value.pop();
+          messages.value.push({ 'message': response.data.response, 'type': 1, 'loading': false, "isMessage": true });
 
-      // Display the chart using the configuration items and data just specified.
-      myChart.setOption(option);
+          const lastMessage = messages.value[messages.value.length - 1];
+      };
+
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+
+    // const chatContainer = document.querySelectorAll('.messages')[0];
+    // chatContainer.scrollTop = chatContainer.scrollHeight
     }
   }
 
   function ajouterDocument() {
     documents.value.push([files.value[files.value.length - 1].name, false]);
-    console.log(files.value[files.value.length - 1], files);
     let formData = new FormData();
     formData.append('file', files.value[files.value.length - 1])
 
@@ -448,8 +289,6 @@
         }
       })
       .then(function (response) {
-        console.log(response);
-        console.log(response.data.res);
         console.log('success');
       }).catch(function (error) {
         console.error(error);
